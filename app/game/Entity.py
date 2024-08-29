09ad85004a8a9a69c9a783f25fd30fb2a7996ebd
lasaqna <nknsyt1@gmail.com>
2024-08-29 18:16:37 +0900

    Entityクラスの作成

--- app/game/Entity.py
+++ app/game/Entity.py
@@ -1,4 +1,23 @@
+from abc import ABC,abstractmethod
 
-
-   
+class Entity(ABC):
+    def __init__(self,size,xposition,yposition,xspeed,yspeed):
+        self._SIZE = size;    
+        self._xposition = xposition
+        self._yposition = yposition
+        self._xspeed = xspeed
+        self._yspeed = yspeed
     
+    def get_position(self):
+        return (self._xposition,self._yposition)
+    
+    def get_speed(self):
+        return (self._xspeed,self._yspeed)
+    
+    def get_size(self):
+        return (self._SIZE)
+    
+    @abstractmethod
+    def move():
+        pass
+        
