076058eb3108d141afa2578dcdb31e7e66d74775
TANSUvsKOYUBI <y.m.w.yuki.kun@gmail.com>
2024-09-05 12:11:31 +0900

    AirHockeyクラスのコンストラクタの作成

---
+++ app/game/AirHockey.py
@@ -0,0 +1,12 @@
+from Pack import Pack
+from Player import Player
+
+class AirHockey:
+    def __init__(self, pack, players, width, height, goal_size, goal_position, finish_point):
+        self._pack = []
+        self._players = []
+        self._width = width
+        self._height = height
+        self._goal_size = goal_size
+        self._goal_position = goal_position
+        self._finish_point = finish_point
