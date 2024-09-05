d4b1dddae8898778f81cd967958ae4d2ef6d5d76
lasaqna <nknsyt1@gmail.com>
2024-09-05 13:00:07 +0900

    ゲーム開始時に伴うパックやプレイヤーの初期化を行う関数を作成

--- app/game/AirHockey.py
+++ app/game/AirHockey.py
@@ -2,6 +2,7 @@ from Pack import Pack
 from Player import Player
 from .. import image_process
 
+
 class AirHockey:
     def __init__(self, pack, players, width, height, goal_size, goal_position, finish_point):
         self._pack = []
@@ -13,3 +14,32 @@ class AirHockey:
         self._finish_point = finish_point
         self.__server =True
 
+
+    def start_game(self):
+        PACKSIZE =20
+        xposition=self._width/2
+        yposition = self._height/2
+        if self.__server:
+            xposition -=xposition/2
+        else :
+            xposition +=xposition/2
+        self.__server= not self.__server
+        pack = Pack(PACKSIZE,xposition,yposition,0,0)
+        self._pack.append(pack)
+
+        PLAYER_SIZE = 20
+        CENTER_LINE = self._width/2
+        players_position_list = image_process()
+        for player_position in players_position_list:
+            HAND_ID = players_position_list[0]
+            (xposition,yposition) = players_position_list[1]
+            TEAM_ID=0
+            if xposition>CENTER_LINE:
+                TEAM_ID=1
+            player = Player(PLAYER_SIZE,xposition,yposition,0,0,TEAM_ID,HAND_ID)
+            self._players.append(player)
+
+
+            
+
+        
