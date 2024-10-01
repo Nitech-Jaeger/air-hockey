b1853c674d07e51061dbc86dede810ef0281fab0
lasaqna <nknsyt1@gmail.com>
2024-10-01 20:00:25 +0900

    ,とその他の演算子の前後の空白の表記方法を統一

--- app/game/AirHockey.py
+++ app/game/AirHockey.py
@@ -18,35 +18,35 @@ class AirHockey:
     # ゲーム開始時に伴う初期化処理
     def start_game(self):
         # パックの初期化
-        PACKSIZE =20
-        xposition=self._width/2
-        yposition = self._height/2
+        PACKSIZE = 20
+        xposition = self._width / 2
+        yposition = self._height / 2
         if self.__server:
-            xposition -=xposition/2
+            xposition -= xposition / 2
         else :
-            xposition +=xposition/2
-        self.__server= not self.__server
-        pack = Pack(PACKSIZE,xposition,yposition,0,0)
+            xposition += xposition / 2
+        self.__server = not self.__server
+        pack = Pack(PACKSIZE, xposition, yposition, 0, 0)
         self._pack.append(pack)
 
         # プレイヤーの初期化
         PLAYER_SIZE = 20
-        CENTER_LINE = self._width/2
+        CENTER_LINE = self._width / 2
         players_position_list = image_process()
         for player_position in players_position_list:
             HAND_ID = player_position[0]
-            (xposition,yposition) = player_position[1]
+            (xposition, yposition) = player_position[1]
             TEAM_ID=0
-            if xposition>CENTER_LINE:
-                TEAM_ID=1
-            player = Player(PLAYER_SIZE,xposition,yposition,0,0,TEAM_ID,HAND_ID)
+            if xposition > CENTER_LINE:
+                TEAM_ID = 1
+            player = Player(PLAYER_SIZE, xposition, yposition, 0, 0, TEAM_ID, HAND_ID)
             self._players.append(player)
         
         return
     
     # タプルの形で渡された２点間の距離を返す
     def __culc_distance_of_two_points(pos1, pos2):
-        dis = (pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2
+        dis = (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
         dis = math.sqrt(dis)
         return dis
     
