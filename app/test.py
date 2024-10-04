c1f2d4e33d7aa8872744b24a798b7174c7cd5626
lasaqna111 <nknsyt1@gmail.com>
2024-10-04 12:16:35 +0900

    1秒間の各エンティティの情報を追えるように変更

--- app/test.py
+++ app/test.py
@@ -7,3 +7,15 @@ GOAL_POSITION = 200
 FINISH_POINT = 5
 
 game  = AirHockey(0,0,WIDTH,HEIGHT,GOAL_POSITION,GOAL_SIZE,FINISH_POINT)
+game.start_game()
+
+li = game.get_entity_list()
+for el in li:
+    print(el)
+
+FRAME = 60
+for i in range(FRAME):
+    game.process_hit_and_move()
+    li = game.get_entity_list()
+    for el in li:
+        print(el)
