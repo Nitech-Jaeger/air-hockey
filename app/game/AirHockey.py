1318a2ee1d372b0eb63d3b0e57a5b4bd0c07b85a
lasaqna111 <nknsyt1@gmail.com>
2024-10-04 13:01:40 +0900

    culcで始まる数学的あ計算を行う関数を外部ファイルに移動

--- app/game/AirHockey.py
+++ app/game/AirHockey.py
@@ -1,5 +1,6 @@
 from .Pack import Pack
 from .Player import Player
+from .culc import *
 # from ..image_process import image_process
 import math
 
@@ -45,91 +46,35 @@ class AirHockey:
         
         return
     
-    # タプルの形で渡された２点間の距離を返す
-    def __culc_distance_of_two_points(self, pos1, pos2):
-        dis = (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
-        dis = math.sqrt(dis)
-        return dis
-    
-    # 点と直線の距離を返す。
-    # 直線はax+by+c=0の形で渡され、点はタプルで渡される。
-    def __culc_distance_of_line_and_point(self, a, b, c, point):
-        temp = a * point[0] + b * point[1] + c 
-        if temp < 0 :
-            temp *= -1
-        return temp / math.sqrt( a ** 2 + b ** 2)
-    
-    # タプルの形式で渡される２点を通る直線の式をax+by+c=0の形で求める。
-    def __culc_linear_function(self, pos1, pos2):
-        if pos1[1] == pos2[1]:
-            a = 0
-            b = 1
-            c = -1 * pos1[1]
-            return a, b, c
-        elif pos1[0] == pos2[0]:
-            a = 1
-            b = 0
-            c = -1 * pos1[0]
-            return a, b, c
-        a = -1 * (pos1[1] - pos2[1])
-        b = pos1[0] - pos2[0]
-        c =-1 * (a * pos1[0] + b *pos1[1])
-        return a, b, c
-
-    #ax+by+c=0と(x-cx)^2+(y-cy)^2=r^2の二つの交点の座標を返す 
-    def __culc_intersection_of_circle_and_linear(self,a, b, c, cx, cy, r):
-        if a == 0:
-            y = -1 * c / b
-            x1 = cx + math.sqrt(r ** 2 - (y - cy) ** 2)
-            x2 = cx - math.sqrt(r ** 2 - (y - cy) ** 2)
-            return (x1, y), (x2, y)
-        elif b == 0:
-            x = -1 * c / b
-            y1 = cy + math.sqrt(r ** 2 - (x - cx) ** 2)
-            y1 = cy - math.sqrt(r ** 2 - (x - cx) ** 2)
-            return (x, y1), (x, y2)
-        y1 = cy + math.sqrt(r ** 2 + (b / a + c / a + cx) ** 2)
-        y2 = cy - math.sqrt(r ** 2 + (b / a + c / a + cx) ** 2)
-        
-        x1 = -1 * (b * y1 + c) / a
-        x2 = -1 * (b * y2 + c) / a
-        return (x1, y1), (x2, y2)
-    
-    # タプル形式で表される２次元ベクトルから外積を計算
-    def __culc_outer_product(self, vec1, vec2):
-        return vec1[0] * vec2[1] - vec1[1] * vec2[0]
-
-        
-    
     # Playerクラスの位置と大きさ、Packクラスの位置と、大きさと速度を渡し、パックの移動中にプレイヤーと衝突するかを判定
     # 衝突すると判明した場合、パックの位置を衝突する瞬間に移動させる。
     # 返り値は修正後のパックの位置情報を格納したタプルで衝突しない場合は負の値が入ったものを返す。
     def __check_hit_with_player(self, pack_speed, pack_position, pack_size, player_position, player_size):
-        (a, b, c) = self.__culc_linear_function(player_position, moved_pack_position)
+        (a, b, c) = culc_linear_function(player_position, moved_pack_position)
         NESS_DIS = pack_size + player_size
         moved_pack_position = (pack_position[0] + pack_speed[0], pack_position[1] + pack_speed[1])
 
-        dis = self.__culc_distance_of_line_and_point(a, b, c, player_position)
+        dis = culc_distance_of_line_and_point(a, b, c, player_position)
         border1 = -b * pack_position[0] + a * pack_position[1] 
         border2 = -b * moved_pack_position[0] + a * moved_pack_position[1] 
         temp = -b * player_position[0] + a * player_position[1]
 
         if dis <= NESS_DIS and (border1 - temp) * (border2 - temp) >= 0:
             # パックの位置の修正
-            (intersec1, intersec2) = self.__culc_intersection_of_circle_and_linear
-            dis1 = self.__culc_distance_of_two_points(pack_position, intersec1)
-            dis2 = self.__culc_distance_of_two_points(pack_position, intersec2)
+            (intersec1, intersec2) = culc_intersection_of_circle_and_linear
+            dis1 = culc_distance_of_two_points(pack_position, intersec1)
+            dis2 = culc_distance_of_two_points(pack_position, intersec2)
             if dis1 < dis2 :
                 return intersec1
             else :
                 return intersec2
         
-        dis  =self.__culc_distance_of_two_points(moved_pack_position, player_position)
+        dis  =culc_distance_of_two_points(moved_pack_position, player_position)
         if dis <= NESS_DIS :
             # パックの位置の修正
-            (intersec1, intersec2) = self.__culc_intersection_of_circle_and_linear
-            dis1 = self.__culc_distance_of_two_points(pack_position, intersec1)
-            dis2 = self.__culc_distance_of_two_points(pack_position, intersec2)
+            (intersec1, intersec2) = culc_intersection_of_circle_and_linear
+            dis1 = culc_distance_of_two_points(pack_position, intersec1)
+            dis2 = culc_distance_of_two_points(pack_position, intersec2)
             if dis1 < dis2 :
                 return intersec1
             else :
@@ -148,10 +93,10 @@ class AirHockey:
         ca = (a[0] - c[0], a[1] - c[1])
         cb = (b[0] - c[0], b[1] - c[1])
 
-        outer_of_ab_and_ac= self.__culc_outer_product(ab, ac)
-        outer_of_ab_and_ad= self.__culc_outer_product(ab, ad)
-        outer_of_cd_and_ca= self.__culc_outer_product(cd, ca)
-        outer_of_cd_and_cb= self.__culc_outer_product(cd, cb)
+        outer_of_ab_and_ac= culc_outer_product(ab, ac)
+        outer_of_ab_and_ad= culc_outer_product(ab, ad)
+        outer_of_cd_and_ca= culc_outer_product(cd, ca)
+        outer_of_cd_and_cb= culc_outer_product(cd, cb)
 
         if outer_of_ab_and_ac * outer_of_ab_and_ad <= 0 and outer_of_cd_and_ca * outer_of_cd_and_cb <= 0:
             return True
@@ -191,13 +136,11 @@ class AirHockey:
         position_after_hit = [pack_position[0], pack_position[1]]
         hit_flag = False
         
-        print(pack_speed)
-
         for i in range(4):
             # 壁と衝突するかの判定
             if self.__check_cross_of_two_linear(pack_position, moved_pack_position, FIELD_POINT[i], FIELD_POINT[(i + 1) % 4]):
                 hit_flag = True
-                (a, b, c) =self.__culc_linear_function(pack_position, moved_pack_position)
+                (a, b, c) =culc_linear_function(pack_position, moved_pack_position)
                 if FIELD_POINT[i][0] == FIELD_POINT[(i + 1) % 4][0]:
                     pack.refrect(False)
                     if b == 0 :
---
+++ app/game/culc.py
@@ -0,0 +1,57 @@
+import math
+
+# タプルの形で渡された２点間の距離を返す
+def culc_distance_of_two_points(pos1, pos2):
+    dis = (pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2
+    dis = math.sqrt(dis)
+    return dis
+
+# 点と直線の距離を返す。
+# 直線はax+by+c=0の形で渡され、点はタプルで渡される。
+def culc_distance_of_line_and_point(a, b, c, point):
+    temp = a * point[0] + b * point[1] + c 
+    if temp < 0 :
+        temp *= -1
+    return temp / math.sqrt( a ** 2 + b ** 2)
+
+# タプルの形式で渡される２点を通る直線の式をax+by+c=0の形で求める。
+def culc_linear_function(pos1, pos2):
+    if pos1[1] == pos2[1]:
+        a = 0
+        b = 1
+        c = -1 * pos1[1]
+        return a, b, c
+    elif pos1[0] == pos2[0]:
+        a = 1
+        b = 0
+        c = -1 * pos1[0]
+        return a, b, c
+    a = -1 * (pos1[1] - pos2[1])
+    b = pos1[0] - pos2[0]
+    c =-1 * (a * pos1[0] + b *pos1[1])
+    return a, b, c
+
+#ax+by+c=0と(x-cx)^2+(y-cy)^2=r^2の二つの交点の座標を返す 
+def culc_intersection_of_circle_and_linear(a, b, c, cx, cy, r):
+    if a == 0:
+        y = -1 * c / b
+        x1 = cx + math.sqrt(r ** 2 - (y - cy) ** 2)
+        x2 = cx - math.sqrt(r ** 2 - (y - cy) ** 2)
+        return (x1, y), (x2, y)
+    elif b == 0:
+        x = -1 * c / b
+        y1 = cy + math.sqrt(r ** 2 - (x - cx) ** 2)
+        y1 = cy - math.sqrt(r ** 2 - (x - cx) ** 2)
+        return (x, y1), (x, y2)
+    y1 = cy + math.sqrt(r ** 2 + (b / a + c / a + cx) ** 2)
+    y2 = cy - math.sqrt(r ** 2 + (b / a + c / a + cx) ** 2)
+    
+    x1 = -1 * (b * y1 + c) / a
+    x2 = -1 * (b * y2 + c) / a
+    return (x1, y1), (x2, y2)
+
+# タプル形式で表される２次元ベクトルから外積を計算
+def culc_outer_product(vec1, vec2):
+    return vec1[0] * vec2[1] - vec1[1] * vec2[0]
+
+    
