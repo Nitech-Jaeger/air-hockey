30a572d5ca3bae9d73e9ca4255f556a72dcca70d
lasaqna111 <nknsyt1@gmail.com>
2024-10-09 13:07:34 +0900

    法線ベクトルを求める方法が間違っていたのを修正

--- app/game/culc.py
+++ app/game/culc.py
@@ -56,11 +56,10 @@ def culc_outer_product(vec1, vec2):
 
 # ２次元空間における二つの座標からその２点を結んだ直線に対する法線ベクトルを計算
 def culc_normal_vec(pos1, pos2):
-    n_x = pos1[0] - pos2[0]
-    n_y = pos1[1] - pos2[1]
-    scale_n = (n_x * n_x + n_y * n_y)
-    scale_n = math.sqrt(scale_n)
-    normal_vec = (n_x / scale_n, n_y / scale_n)
+    (a, b, c) = culc_linear_function(pos1, pos2)
+    scale = math.sqrt(a * a + b* b)
+    normal_vec = (a / scale, b / scale)
+    
     return normal_vec
 
 # タプル形式で表される２次元ベクトルから内積を計算
