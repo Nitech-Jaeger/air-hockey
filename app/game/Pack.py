237106995a1444bac3d044ae3e1f5721041234ab
lasaqna111 <nknsyt1@gmail.com>
2024-10-10 16:29:16 +0900

    パックとプレイヤー間での非弾性衝突の処理が間違っていたのを修正

--- app/game/Pack.py
+++ app/game/Pack.py
@@ -24,23 +24,35 @@ class Pack(Entity):
         return 
 
 
-    # packとplayerの完全弾性衝突による衝突後の速度を計算
+    # packとplayerの非弾性衝突による衝突後の速度を計算
     def __elastic_collsion(self, player_speed, player_position):
+        # 跳ね返り係数
+        e = 0.5
+
         pack_position = (self._xposition, self._yposition)
         pack_speed = (self._xspeed, self._yspeed)
-        n_vec = culc_normal_vec(pack_position, player_position)
-        t_vec = (-1 * n_vec[1], n_vec[0])
+        # tが接線方向、nがそれの法線方向の単位ベクトル
+        t_vec = culc_normal_vec(pack_position, player_position)
+        n_vec = (-1 * t_vec[1], t_vec[0])
 
-        v_pack_n = culc_inner_product(pack_speed, n_vec)
-        v_pack_t = culc_inner_product(pack_speed, t_vec)
-        v_player_n = culc_inner_product(player_speed, n_vec)
-        v_player_t = culc_inner_product(player_speed, t_vec)
+        # 接線方向とその法線方向の正射影ベクトルを求める
+        temp = culc_inner_product(t_vec, pack_speed)
+        pack_speed_t = (temp * t_vec[0], temp * t_vec[1])
+        pack_speed_n = (pack_speed[0] - pack_speed_t[0], pack_speed[1] - pack_speed_t[1])
+        temp = culc_inner_product(t_vec, player_speed)
+        player_speed_t = (temp * t_vec[0], temp * t_vec[1])
+        player_speed_n = (player_speed[0] -player_speed_t[0], player_speed[1] - player_speed_t[1])
 
+        # プレイヤーとパックの法線方向における弾性衝突語の速度を計算
+        temp1 = ((1 + e) * player_speed_n[0] + (e -1) * pack_speed_n[0]) / (2 * e)
+        temp2 = ((1 + e) * player_speed_n[1] + (e -1) * pack_speed_n[1]) / (2 * e)
+        hitted_pack_speed_n = (temp1, temp2)
+        hitted_pack_speed_t = pack_speed_t
 
-        V_pack_x = v_player_n * n_vec[0] + v_pack_t * t_vec[0]
-        V_pack_y = v_player_n * n_vec[1] + v_pack_t * t_vec[1]
+        # xy平面上のベクトルに戻す
+        hitted_pack_speed = (hitted_pack_speed_n[0] + hitted_pack_speed_t[0], hitted_pack_speed_n[1] + hitted_pack_speed_t[1])
 
-        return (V_pack_x, V_pack_y)
+        return hitted_pack_speed
 
     # プレイヤーと衝突したときの処理
     def hit_player(self, player_speed, player_position):
